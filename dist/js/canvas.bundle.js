e85924ee88a8cd5b3637fd40f7f5c87badbab84e
razalrahmanp <razalrahmanp@gmail.com>
2022-07-18 17:20:05 +0530

    setup

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -184,25 +184,7 @@ animate();
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function randomColor(colors) {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function distance(x1, y1, x2, y2) {
-  var xDist = x2 - x1;
-  var yDist = y2 - y1;
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-}
-
-module.exports = {
-  randomIntFromRange: randomIntFromRange,
-  randomColor: randomColor,
-  distance: distance
-};
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'G:\\Razal\\WEB\\Game\\mario\\src\\js\\utils.js'");
 
 /***/ })
 
